99d3da3fc0f8b7acdf42ad1e4ceb4c6a9db0dadc
iRequire <pl@patricklist.dev>
2021-12-21 18:13:51 +0100

    replace backend with lumen (laravel)

--- graphql/src/types/ServerStatsContext.ts
+++
@@ -1,9 +0,0 @@
-
-import { Request, Response } from "express";
-import { ServerStatsJwtPayload } from "./ServerStatsJwtPayload";
-
-export interface ServerStatsContext {
-  req: Request;
-  res: Response;
-  user?: ServerStatsJwtPayload;
-}
--- graphql/src/util/ConfigUtil.ts
+++
@@ -1,4 +0,0 @@
-
-export class ConfigUtil {
-    static DEFAULT_JWT_SECRET = 'labmon';
-  }
