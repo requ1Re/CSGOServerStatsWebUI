b3d8874b52bdb1a85c219b7306e5749300f691b9
iRequire <pl@patricklist.dev>
2021-12-22 17:42:18 +0100

    Frontend: Small UI update (favicon & server display)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title = '1337Frankfurt CS:GO Servers';
+
+  ngOnInit(): void {
+      document.title = this.title;
+  }
 }
