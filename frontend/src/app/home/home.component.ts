71197a9bf31297ec2f099c748d735ba2b11e2c9e
iRequire <pl@patricklist.dev>
2021-12-22 17:30:51 +0100

    Frontend: Added server display

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCopy, faPlayCircle } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  faCopy = faCopy;
+  faPlayCircle = faPlayCircle;
+
+  gamemodes: ServerDisplay[] = [
+    {
+      name: 'Retakes #1',
+      imgPath: '/assets/img/retakes.jpg',
+      ip: '94.130.22.111:27015'
+    },
+    {
+      name: 'KZ #1',
+      imgPath: '/assets/img/kz.jpg',
+      ip: '94.130.22.111:27016'
+    },
+    {
+      name: 'Surf #1',
+      imgPath: '/assets/img/surf_utopia_v3.jpg',
+      ip: '94.130.22.111:27017'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  copyToClipboard(ip: string){
+    navigator.clipboard.writeText(ip)
+        .then(() => {
+        console.log("Copied IP!")
+    })
+        .catch(err => {
+        console.log('Something went wrong', err);
+    })
+  }
+
+}
+
+interface ServerDisplay {
+  name: string;
+  imgPath: string;
+  ip: string;
 }
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SafeUrlPipe } from './pipes/safe-url.pipe';
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent],
+  declarations: [NavbarComponent, FooterComponent, SafeUrlPipe],
   imports: [CommonModule, RouterModule, FontAwesomeModule],
-  exports: [NavbarComponent, FooterComponent, FontAwesomeModule],
+  exports: [NavbarComponent, FooterComponent, FontAwesomeModule, SafeUrlPipe],
 })
 export class SharedModule {}
