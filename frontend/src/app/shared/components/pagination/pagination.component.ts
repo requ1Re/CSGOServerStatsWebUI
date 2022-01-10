3dfcbaf7a0259bfcef35574cefc1533d95b902cf
iRequire <pl@patricklist.dev>
2022-01-10 17:36:21 +0100

    frontend: pagination ellipsis algorithm

--- frontend/src/app/shared/components/pagination/pagination.component.ts
+++ frontend/src/app/shared/components/pagination/pagination.component.ts
@@ -35,4 +35,39 @@ export class PaginationComponent implements OnInit {
   isActive(page: number) {
     return this.page === page;
   }
+
+  pagination(current: number, lastNumber: number) {
+    var current = current,
+      last = lastNumber,
+      delta = 2,
+      left = current - delta,
+      right = current + delta + 1,
+      range = [],
+      rangeWithDots = [],
+      l;
+
+    for (let i = 1; i <= last; i++) {
+      if (i == 1 || i == last || (i >= left && i < right)) {
+        range.push(i);
+      }
+    }
+
+    for (let i of range) {
+      if (l) {
+        if (i - l === 2) {
+          rangeWithDots.push(l + 1);
+        } else if (i - l !== 1) {
+          rangeWithDots.push('...');
+        }
+      }
+      rangeWithDots.push(i);
+      l = i;
+    }
+
+    console.log(rangeWithDots);
+    return rangeWithDots;
+  }
+
+  isNumber(val: any): boolean { return typeof val === 'number'; }
+  parseNumber(val: any) { return parseInt(val); }
 }
