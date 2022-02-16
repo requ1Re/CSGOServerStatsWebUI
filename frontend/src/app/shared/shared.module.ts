cc3ddb1157dc272d12a70256bedef8df276e0ab6
require7 <patrick.list@pm.me>
2022-02-16 08:42:47 +0100

    moved loading overlay to seperate component

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -10,10 +10,28 @@ import { HttpClientModule } from '@angular/common/http';
 import { BaseComponent } from './components/base/base.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
+import { LoadingOverlayComponent } from './components/loading-overlay/loading-overlay.component';
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent, SafeUrlPipe, BaseComponent, PaginationComponent, LoadingSpinnerComponent],
+  declarations: [
+    NavbarComponent,
+    FooterComponent,
+    SafeUrlPipe,
+    BaseComponent,
+    PaginationComponent,
+    LoadingSpinnerComponent,
+    LoadingOverlayComponent,
+  ],
   imports: [CommonModule, RouterModule, FontAwesomeModule, HttpClientModule],
-  exports: [NavbarComponent, FooterComponent, FontAwesomeModule, SafeUrlPipe, BaseComponent, PaginationComponent, LoadingSpinnerComponent],
+  exports: [
+    NavbarComponent,
+    FooterComponent,
+    FontAwesomeModule,
+    SafeUrlPipe,
+    BaseComponent,
+    PaginationComponent,
+    LoadingSpinnerComponent,
+    LoadingOverlayComponent,
+  ],
 })
 export class SharedModule {}
